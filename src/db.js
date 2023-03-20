be681c72109cf967c926c5875aa2e4643b8ec600
carlosGabrielCarreno <carrenocarlosgabriel@gmail.com>
2023-03-20 17:40:42 -0300

    add some changes

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 let sequelize =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
-        dialect: 'postgres',
+        dialect: "postgres",
         host: DB_HOST,
         port: DB_PORT,
         username: DB_USER,
@@ -32,18 +32,28 @@ let sequelize =
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
+
+async function connectToDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
     (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -62,10 +72,11 @@ const { Recipe, TypeOfDiet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Recipe.belongsToMany(TypeOfDiet, { through: 'recipe-typeOfDiet' });
-TypeOfDiet.belongsToMany(Recipe, { through: 'recipe-typeOfDiet' });
+Recipe.belongsToMany(TypeOfDiet, { through: "recipe-typeOfDiet" });
+TypeOfDiet.belongsToMany(Recipe, { through: "recipe-typeOfDiet" });
 
 module.exports = {
+  connectToDatabase,
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
