4d2aa922ac2ddf4fc71430c6d6eb7da78f25601c
carlosGabrielCarreno <carrenocarlosgabriel@gmail.com>
2023-03-21 11:33:09 -0300

    deleted data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { loadRecipesInTheDb } = require("./src/helpers/loadRecipesInTheDb.js");
-const DB_PORT = process.env.DB_PORT;
+const DB_PORT = process.env.DB_PORT || 3000;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   //await loadRecipesInTheDb();
   server.listen(DB_PORT, () => {
     console.log("%s listening at " + DB_PORT); // eslint-disable-line no-console
